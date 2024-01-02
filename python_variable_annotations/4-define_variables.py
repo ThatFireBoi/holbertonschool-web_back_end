3bc1e978f711574371271678d16d0e331f4c8073
ThatFireBoi <gcf2007@hotmail.com>
2024-01-02 18:40:30 -0400

    Defines and annotates some variables for specified values

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Define and annotate the following variables with the specified values:
+a, an integer with a value of 1
+pi, a float with a value of 3.14
+i_understand_annotations, a boolean with a value of True
+school, a string with a value of Holberton"""
+
+
+a: int = 1
+"""An integer with a value of 1"""
+
+pi: float = 3.14
+"""A float with a value of 3.14"""""
+
+i_understand_annotations: bool = True
+"""A boolean with a value of True"""
+
+school: str = "Holberton"
+"""A string with a value of Holberton"""
