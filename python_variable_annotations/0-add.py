bb4770c9705e01a985d1df64c095f0311d405479
ThatFireBoi <gcf2007@hotmail.com>
2024-01-02 18:05:09 -0400

    Type-annotated function add, float a&b as arguments

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Type-annotated function add that takes a
+float a and a float b as arguments"""
+
+
+def add(a: float, b: float) -> float:
+    """Add two floats and return the result as a float"""
+    return a + b
