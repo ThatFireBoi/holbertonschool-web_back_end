eef86130fc3fca18f67a50da25aedf031ab01d80
ThatFireBoi <gcf2007@hotmail.com>
2024-01-04 15:02:20 -0400

    Correction to async function

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -9,6 +9,7 @@ from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-def task_wait_n(n: int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """Return a list of all the delays (float values)"""
-    return asyncio.run(task_wait_random(n, max_delay))
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
