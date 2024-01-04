aa93f9bbcb780f9352cf2e9f2a96b85e0e07f742
ThatFireBoi <gcf2007@hotmail.com>
2024-01-04 11:12:47 -0400

    Fixes to asynchronous runtime

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -2,14 +2,16 @@
 """Module containing measure_runtime coroutine"""
 
 
+import asyncio
 import time
 from typing import List
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_runtime(n: int, max_delay: int) -> float:
-    """Return the total runtime"""
-    start_time = time.time()
-    await wait_n(n, max_delay)
-    end_time = time.time()
-    return (end_time - start_time) / n
+async def measure_time(n: int, max_delay: int) -> float:
+    """Return the average time for wait_n to execute n times with the
+    specified max_delay"""
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+    return (end - start) / n
