11929e1ecd6ecb043250b630afba585fe51896de
ThatFireBoi <gcf2007@hotmail.com>
2024-01-12 18:15:14 -0400

    Fixes to get_hyper

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -37,12 +37,11 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """Return a list of lists containing the data for the requested
-        page."""
+        """Return the appropriate page of the dataset"""
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
-        start_index, end_index = index_range(page, page_size)
-        return self.dataset()[start_index:end_index]
+        range = index_range(page, page_size)
+        return self.dataset()[range[0]:range[1]]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         """Return a dictionary containing the following key-value pairs"""
@@ -56,5 +55,5 @@ class Server:
             'data': data,
             'next_page': page + 1 if page < total_pages else None,
             'prev_page': page - 1 if page > 1 else None,
-            'total_pages': total_pages,
+            'total_pages': int(total_pages)
         }
