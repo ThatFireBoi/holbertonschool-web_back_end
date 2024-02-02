f686d56b9c7e8c8df4b914bdb0af1a8c426a0d68
ThatFireBoi <gcf2007@hotmail.com>
2024-02-01 21:10:03 -0400

    Fix to students argument

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,13 +1,9 @@
-export default function updateStudentGradeByCity(array, city, newGrades) {
-  return array
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
     .filter((student) => student.location === city)
     .map((student) => {
-      const grade = newGrades.filter((grade) => grade.studentId === student.id);
-      if (grade.length === 0) {
-        student.grade = 'N/A';
-      } else {
-        student.grade = grade[0].grade;
-      }
-      return student;
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      const grade = gradeObj ? gradeObj.grade : 'N/A';
+      return { ...student, grade };
     });
 }
