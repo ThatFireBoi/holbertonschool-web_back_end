5281488866292d64417d07aa99cdc1cb0492389d
ThatFireBoi <gcf2007@hotmail.com>
2024-02-01 22:12:31 -0400

    Fix to return statement

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,12 @@
 export default function cleanSet(set, startString) {
-  if (!startString) {
+  if (startString === '' || typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
-  return [...set]
-    .filter((string) => string.startsWith(startString))
-    .map((string) => string.slice(startString.length))
-    .join('-');
+  const str = [];
+  set.forEach((element) => {
+    if (element && element.startsWith(startString)) {
+      str.push(element.slice(startString.length));
+    }
+  });
+  return str.join('-');
 }
